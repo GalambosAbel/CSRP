8a33a418b39c6719cc9f61913b39a7223739940a
GalambosAbel <abel.galambos@gmail.com>
2024-10-06 23:39:26 +0200

    the c++ part can now be used from outside

--- cpp stuff/main.cpp
+++ cpp stuff/main.cpp
@@ -10,9 +10,9 @@
 
 using namespace std;
 
-void tspToBmp(string fileNameNoExtension, double maxDist = 60000) {
-    SquareMatrixF distanceMatrix = GraphReader::readTsp_Explicit_FullMatrix((fileNameNoExtension + ".tsp").data());
-    vector<int> order = GraphReader::readNeosTour((fileNameNoExtension + ".tour").data());
+void tspToBmp(char* tspFileName, char* tourFileName, char* imgFileName, double maxDist = 60000) {
+    SquareMatrixF distanceMatrix = GraphReader::readTsp_Explicit_FullMatrix(tspFileName);
+    vector<int> order = GraphReader::readNeosTour(tourFileName);
     distanceMatrix.order(order.data());
     
     //puts the dummy on the edge
@@ -20,16 +20,16 @@ void tspToBmp(string fileNameNoExtension, double maxDist = 60000) {
     for (int i = 0; i < order.size(); i++)
     {
         if (order[i] == order.size() - 1) {
-            cout << i;
+            // cout << i;
             offset = order.size() - i;
         }
     }
     
-    distanceMatrix.toImage(maxDist, ColorScheme::spectral(), offset, false, true).printImageAsBMP((fileNameNoExtension + ".png").data());
+    distanceMatrix.toImage(maxDist, ColorScheme::spectral(), offset, false, true).printImageAsBMP(imgFileName);
 }
 
-double inToTsp(string fileNameNoExtension, int matrixSize) {
-    FILE* file = fopen((fileNameNoExtension + ".in").data(), "r");
+double inToTsp(char* inFileName, char* tspFileName, int matrixSize) {
+    FILE* file = fopen(inFileName, "r");
 
     //build distance matrix
     SquareMatrixF distanceMatrix(matrixSize + 1);
@@ -52,15 +52,42 @@ double inToTsp(string fileNameNoExtension, int matrixSize) {
         distanceMatrix.setElement(matrixSize, i, 0);
         distanceMatrix.setElement(i, matrixSize, 0);
     }
-    distanceMatrix.toTspFullMatrix((fileNameNoExtension + ".tsp").data());
+    distanceMatrix.toTspFullMatrix(tspFileName);
     return maxDist;
 } 
 
-int main ()
-{
-    // cout << inToTsp("..\\work_layer_0", 1305);
-    tspToBmp("..\\work_layer_0", 120000); //maxDist is 117767
 
-    // runAllTests();
+int main (int argc, char* argv[])
+{
+    if (argc == 1) {
+        //Sinec test file paths are hardcoded, this needs to be invoked from the correct folder (same as source code)
+        runAllTests();
+        return 0;
+    }
+    else if (argc >= 5 && (stricmp(argv[1], "inToTsp") == 0 || stricmp(argv[1], "itt") == 0)) {
+        int matrixSize = stoi(argv[4]);
+        int maxDist = inToTsp(argv[2], argv[3], matrixSize);
+        if(argc == 6) {
+            if(stricmp(argv[5], "print") == 0) {
+                cout << maxDist;
+            }
+            if(stricmp(argv[5], "return") == 0) {
+                return maxDist;
+            }
+        }
+        return 0;
+    }
+    else if (argc >= 5 && (stricmp(argv[1], "tspToImg") == 0 || stricmp(argv[1], "tti") == 0)) {
+        if(argc == 6) {
+            int maxDist = stoi(argv[5]);
+            tspToBmp(argv[2], argv[3], argv[4], maxDist);
+        }
+        else {
+            tspToBmp(argv[2], argv[3], argv[4]);
+        }
+        return 0;
+    }
+    return -1;
 }
+
 
